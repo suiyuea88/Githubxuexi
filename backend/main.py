b55049a59971127226e47fec99cb6ff25f52934e
suiyuea88 <wangzheguoji88@gmail.com>
2026-07-15 21:52:21 +0800

    Add files via upload

---
+++ backend/main.py
@@ -0,0 +1,36 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from backend.github_api import get_hot_projects
+
+
+app = FastAPI(
+    title="GitHub每日精选"
+)
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+
+@app.get("/")
+def home():
+
+    return {
+        "message":
+        "GitHub每日精选运行成功"
+    }
+
+
+
+@app.get("/projects")
+def projects():
+
+    data = get_hot_projects()
+
+    return data

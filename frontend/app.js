5b6370927c197f33a297e1cc5843752643c1cb5f
suiyuea88 <wangzheguoji88@gmail.com>
2026-07-15 22:07:04 +0800

    Update app.js

--- frontend/app.js
+++ frontend/app.js
@@ -1,103 +1,91 @@
 const API =
-"https://https://githubxuexi.onrender.com/projects";
-
+"https://githubxuexi.onrender.com/projects";
 
 
 fetch(API)
 
-.then(res=>res.json())
+.then(res => res.json())
 
-.then(data=>{
-
-
-let html="";
+.then(data => {
 
 
-data.forEach(project=>{
+    let html = "";
 
 
-html+=`
-
-<div class="card">
+    data.forEach(project => {
 
 
-<h2>
-🔥 ${project.name}
-</h2>
+        html += `
+
+        <div class="card">
 
 
-<p>
-👤 作者：
-${project.author}
-</p>
+            <h2>
+            🔥 ${project.name}
+            </h2>
 
 
-<p class="star">
-
-⭐ Star:
-${project.stars}
-
-</p>
+            <p>
+            👤 作者：
+            ${project.author}
+            </p>
 
 
-
-<p>
-
-💻 语言：
-
-${project.language}
-
-</p>
+            <p class="star">
+            ⭐ Star：
+            ${project.stars}
+            </p>
 
 
-<hr>
+            <p>
+            💻 技术语言：
+            ${project.language}
+            </p>
 
 
-<h3>
-中文简介
-</h3>
+            <hr>
 
 
-<p>
-
-${project.translation}
-
-</p>
+            <h3>
+            📖 项目简介
+            </h3>
 
 
+            <p>
+            ${project.translation}
+            </p>
 
-<a href="${project.url}" target="_blank">
 
-查看源码
+            <a href="${project.url}" target="_blank">
+            🔗 查看源码
+            </a>
 
-</a>
+
+        </div>
+
+        `;
+
+
+    });
 
 
 
-</div>
-
-
-`;
-
-
-
-});
-
-
-
-document.getElementById(
-"projects"
-).innerHTML=html;
+    document.getElementById(
+        "projects"
+    ).innerHTML = html;
 
 
 
 })
 
-.catch(err=>{
 
-document.getElementById(
-"projects"
-).innerHTML=
-"加载失败";
+.catch(error=>{
+
+
+    document.getElementById(
+        "projects"
+    ).innerHTML =
+    "加载失败，请检查接口";
+
 
 });
